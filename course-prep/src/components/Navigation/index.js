7ea8a663fc521bfaf10731ebc82ee2da3ff578cf
Parker Ford <fordp2@wwu.edu>
2020-06-02 12:26:16 -0700

    connecting database

---
+++ course-prep/src/components/Navigation/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import SignOutButton from '../SignOut';
+import * as ROUTES from '../../constants/routes';
+import { AuthUserContext } from '../Session';
+
+const Navigation = () => (
+    <div>
+        <AuthUserContext.Consumer>
+            {authUser =>
+                authUser ? <NavigationAuth /> : <NavigationNonAuth/>
+            }
+        </AuthUserContext.Consumer>
+     </div>
+)
+
+const NavigationAuth = () => (
+    <div>
+        <ul>
+            <li>
+                <Link to={ROUTES.LANDING}>Landing</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.USER_TEST}>Users</Link>
+            </li>
+            <li>
+                <SignOutButton />
+            </li>
+        </ul>
+    </div>
+);
+
+const NavigationNonAuth = () => (
+    <ul>
+        <li>
+            <Link to={ROUTES.LANDING}>Landing</Link>
+        </li>
+        <li>
+            <Link to={ROUTES.LOG_IN}>Login</Link>
+        </li>
+    </ul>
+)
+
+export default Navigation;
---
+++ course-prep/src/constants/routes.js
@@ -0,0 +1,4 @@
+export const LANDING = '/';
+export const SIGN_UP = '/signup';
+export const LOG_IN = '/login';
+export const USER_TEST = '/user_test'
