8331188d6929b12384c450ac9b7cd410e5cad13d
pratzm <masonpratz@gmail.com>
2020-11-19 14:13:12 -0800

    close button fix

--- src/components/CreateQuestionButton/index.js
+++ src/components/CreateQuestionButton/index.js
@@ -18,7 +18,7 @@ function CreateQuestionButton () {
     var modal;
     useEffect( () => {
         modal = document.getElementById("questionModal");
-    }, [])
+    }, )
     if(modal)(
         console.log("modal not null")
     )
@@ -37,6 +37,7 @@ function CreateQuestionButton () {
 
     function handleSpanClick(){
         modal.style.display = "none";
+        setTypeSelected("")
     }
     function handleSubmit(){
 
